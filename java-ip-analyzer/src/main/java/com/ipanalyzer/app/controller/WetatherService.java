64f77bed87cdc3e74794086a30ffd88742642988
郭敬冬 <guo_jingdong@topsec.com.cn>
2025-04-01 17:13:48 +0800

    支持ai mcp

--- java-ip-analyzer/src/main/java/com/ipanalyzer/app/controller/WetatherService.java
+++ java-ip-analyzer/src/main/java/com/ipanalyzer/app/controller/WetatherService.java
@@ -1,23 +1,23 @@
-//package com.ipanalyzer.app.controller;
-//
-//import org.springframework.ai.tool.ToolCallbackProvider;
-//import org.springframework.ai.tool.annotation.Tool;
-//import org.springframework.ai.tool.annotation.ToolParam;
-//import org.springframework.ai.tool.method.MethodToolCallbackProvider;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-//@Service
-//public class WetatherService {
-//    @Tool(description = "根据城市名称获取天气信息")
-//    public String getWertherCity(@ToolParam(description = "城市名称") String city
-//            , @RequestParam(defaultValue = "市区") String area) {
-//        return "晴朗";
-//    }
-//
-//    @Bean
-//    public ToolCallbackProvider weatherTools(WetatherService wetatherService) {
-//        return MethodToolCallbackProvider.builder().toolObjects(wetatherService).build();
-//    }
-//}
+package com.ipanalyzer.app.controller;
+
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Service
+public class WetatherService {
+    @Tool(description = "根据城市名称获取天气信息")
+    public String getWertherCity(@ToolParam(description = "城市名称") String city
+            , @RequestParam(defaultValue = "市区") String area) {
+        return "晴朗";
+    }
+
+    @Bean
+    public ToolCallbackProvider weatherTools(WetatherService wetatherService) {
+        return MethodToolCallbackProvider.builder().toolObjects(wetatherService).build();
+    }
+}
